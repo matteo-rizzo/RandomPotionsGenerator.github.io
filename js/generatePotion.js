dd2689a8a819512e8b5e67ca6d62f540560fe600
Matte <matteo96rizzo@gmail.com>
2018-08-10 15:11:12 +0200

    Implemented logic

--- js/generatePotion.js
+++ js/generatePotion.js
@@ -1,3 +1,23 @@
+function loadJSON(callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', './data.json', true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        callback(JSON.parse(xobj.responseText));
+      }
+    };
+    xobj.send(null);  
+  }
+
 function generatePotion() {
-    
+    loadJSON(function(json) {
+        // console.log(JSON.stringify(json, null, 2)); 
+        
+        var potions = json.potions;
+        var selectedPotion = Math.floor((Math.random() * (potions.length - 1)) + 1)
+
+        $('#potion-name').text(potions[selectedPotion].name);
+        $('#potion-description').text(potions[selectedPotion].description);
+      });
 }
